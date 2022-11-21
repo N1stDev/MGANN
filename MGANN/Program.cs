ac8d90ce38271c3049a0a9316e757830c5dc086e
losdayver <99668293+losdayver@users.noreply.github.com>
2022-11-21 17:47:02 +0300

    Эксперементирую с матрицами из библиотеки MathNet Numerics

--- MGANN/Program.cs
+++ MGANN/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using MathNet.Numerics.LinearAlgebra.Double;
+
+class NetWork
+{
+    
+}
+
+class Program
+{
+    static void Main()
+    {
+        var vector1 = Vector.Build.Dense(3);
+        vector1[0] = 5;
+        vector1[1] = 3;
+        vector1[2] = 1;
+
+        var matrix1 = Matrix.Build.Dense(3, 3);
+        matrix1[0, 0] = 1;
+        matrix1[1, 1] = 3;
+        matrix1[2, 2] = 2;
+        var vector2 = matrix1 * vector1;
+
+        Console.WriteLine(matrix1);
+        Console.WriteLine(vector2);
+    }
+}
