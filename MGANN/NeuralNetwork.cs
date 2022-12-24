8af2f142992923c974ed1d7f4065678f05d27192
N1stDev <nist.dev@yandex.ru>
2022-12-24 18:44:45 +0300

    переделал под CNN

---
+++ MGANN/NeuralNetwork.cs
@@ -0,0 +1,86 @@
+﻿
+namespace MGANN
+{
+    class CNN
+    {
+        void ForwardPropogation()
+        {
+
+        }
+
+        void BackPropogation()
+        {
+
+        }
+
+        void Train()
+        {
+
+        }
+    }
+
+    class ConvolutionLayer
+    {
+        public ConvolutionLayer()
+        {
+
+        }
+
+        void PatchesGenerator()
+        {
+
+        }
+
+        void ForwardPropogation()
+        {
+
+        }
+
+        void BackPropogation()
+        {
+
+        }
+    }
+
+    class MaxPoolingLayer
+    {
+        public MaxPoolingLayer()
+        {
+
+        }
+
+        void PatchesGenerator()
+        {
+
+        }
+
+        void ForwardPropogation()
+        {
+
+        }
+
+        void BackPropogation()
+        {
+
+        }
+
+    }
+
+    class SoftMaxLayer
+    {
+        public SoftMaxLayer()
+        {
+
+        }
+
+        void ForwardPropogation()
+        {
+
+        }
+
+        void BackPropogation()
+        {
+
+        }
+    }
+}
